13742cb3b6a461f192ce2da6cf94ba8d74190909
Gaudy035 <miloszgaudy@gmail.com>
2026-06-30 19:36:40 +0200

    initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='bg-slate-800 flex justify-center items-center min-h-screen w-screen'>
+      <h1 className='font-semibold flex-1 flex justify-center items-center h-full w-full text-2xl text-white'>
+        Hello
+      </h1>
+    </div>
+  );
+}
