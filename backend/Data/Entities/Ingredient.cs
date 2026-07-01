131fe8d41a1a9fbb47bb2640c19a8f1de02171ea
Gaudy035 <miloszgaudy@gmail.com>
2026-07-01 20:19:10 +0200

    feat(backend): Add ingredient entity

---
+++ backend/Data/Entities/Ingredient.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Data.Entities;
+
+[Table("t_ingredient")]
+public class Ingredient
+{
+    [Key]
+    [Column("ingredient_id")]
+    public int IngredientId { get; set; }
+
+    [Required]
+    [MaxLength(128)]
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [Column("allergen")]
+    public bool Allergen { get; set; }
+}
