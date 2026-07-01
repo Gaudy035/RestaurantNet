c42c24f6735ff262356b341d13bda0c82c7a76af
Gaudy035 <miloszgaudy@gmail.com>
2026-07-01 16:36:07 +0200

    feat(backend): Add Client database entity

---
+++ backend/Data/Entities/Client.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Data.Entities;
+
+[Table("t_client")]
+public class Client
+{
+    [Key]   
+    [Column("user_id")]
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    [Column("phone_number")]
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    [ForeignKey("UserId")]
+    public User User { get; set; } = null!;
+}
