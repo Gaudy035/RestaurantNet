5ffd64662c6096345d556b7a91b31c57dc7ef2c4
Gaudy035 <miloszgaudy@gmail.com>
2026-07-01 17:57:43 +0200

    feat(backend): Add location/employee config

---
+++ backend/Data/Configurations/LocationEmployeeConfiguration.cs
@@ -0,0 +1,23 @@
+using backend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace backend.Data.Configurations;
+
+public class LocationEmployeeConfiguration : IEntityTypeConfiguration<LocationEmployee>
+{
+    public void Configure(EntityTypeBuilder<LocationEmployee> builder)
+    {
+        builder.HasKey(le => new { le.UserId, le.LocationId });
+
+        builder.HasOne(le => le.Employee)
+            .WithMany(e => e.LocationEmployees)
+            .HasForeignKey(le => le.LocationId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(le => le.Location)
+            .WithMany(l => l.LocationEmployees)
+            .HasForeignKey(le => le.LocationId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
