46823d91cc496e92df8f96dd7f10d2e231080922
Gaudy035 <miloszgaudy@gmail.com>
2026-07-02 18:33:24 +0200

    feat(backend): Add DbSets to AppDbContext

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -13,6 +13,14 @@ public class AppDbContext: DbContext
     public DbSet<Employee> Employees => Set<Employee>();
     public DbSet<Location> Locations => Set<Location>();
     public DbSet<LocationEmployee> LocationEmployees => Set<LocationEmployee>();
+    public DbSet<Table> Tables => Set<Table>();
+    public DbSet<Booking> Bookings => Set<Booking>();
+    public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+    public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+    public DbSet<ItemIngredient> ItemIngredients => Set<ItemIngredient>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<Delivery> Deliveries => Set<Delivery>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
