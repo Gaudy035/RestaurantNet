34a9e95e7587117b487f1d9a09e4c461fded4a8a
Gaudy035 <miloszgaudy@gmail.com>
2026-07-04 21:15:02 +0200

    feat(backend): Add client and employee account creation from admin

---
+++ backend/Controllers/Admin/AdminUserController.cs
@@ -0,0 +1,46 @@
+using backend.DTOs.Users;
+using backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers.Admin;
+
+[ApiController]
+[Route("admin/user")]
+public class AdminUserController: ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AdminUserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost("employees")]
+    public async Task<IActionResult> CreateEmployeeAccount([FromBody] CreateEmployeeDto dto)
+    {
+        var newEmployee = await _userService.CreateEmployee(dto);
+
+        if (newEmployee == null)
+        {
+            return BadRequest(new { detail = "Failed to create employee account" });
+        }
+
+        return Ok(newEmployee);
+    }
+
+    [Authorize(Roles = "Admin,Employee")]
+    [HttpPost("clients")]
+    public async Task<IActionResult> CreateClientAccount([FromBody] CreateClientDto dto)
+    {
+        var newClient = await _userService.CreateClient(dto);
+
+        if (newClient == null)
+        {
+            return BadRequest(new { detail = "Failed to create client account" });
+        }
+
+        return Ok(newClient);
+    }
+}
