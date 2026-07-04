3dddfc02ef9a342b48290bb39c91ec2e0c4d042a
Miłosz Gaudy <miloszgaudy@gmail.com>
2026-07-04 23:00:37 +0200

    style(backend): Add missing newlines in user creation response dtos

--- backend/DTOs/Users/CreateClientResponseDto.cs
+++ backend/DTOs/Users/CreateClientResponseDto.cs
@@ -3,6 +3,7 @@ namespace backend.DTOs.Users;
 public class CreateClientResponseDto
 {
     public int UserId { get; set; }
+    
     public string FirstName { get; set; } = string.Empty;
     
     public string LastName { get; set; } = string.Empty;
