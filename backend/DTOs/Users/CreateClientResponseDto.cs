f4181ece1fda8bb5babbc7a472b0ba9067b3d114
Gaudy035 <miloszgaudy@gmail.com>
2026-07-04 21:20:04 +0200

    feat(backend): Add user creation dtos

---
+++ backend/DTOs/Users/CreateClientResponseDto.cs
@@ -0,0 +1,13 @@
+namespace backend.DTOs.Users;
+
+public class CreateClientResponseDto
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+}
---
+++ backend/DTOs/Users/CreateEmployeeResponseDto.cs
@@ -0,0 +1,13 @@
+namespace backend.DTOs.Users;
+
+public class CreateEmployeeResponseDto
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public bool IsAdmin { get; set; }
+}
