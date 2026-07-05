d51449d19c44fd474e982ee262490b2078a7d2b6
Gaudy035 <miloszgaudy@gmail.com>
2026-07-05 18:25:36 +0200

    fix(backend): Add mising scoped service

--- backend/Program.cs
+++ backend/Program.cs
@@ -70,6 +70,7 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddAuthorization();
 
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
