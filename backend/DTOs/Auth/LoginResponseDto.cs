b4f7bab14ca604452b739d821d035880eeeb5e60
Gaudy035 <miloszgaudy@gmail.com>
2026-07-07 11:18:28 +0200

    feat(backend): Add login request and response dtos

---
+++ backend/DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+namespace backend.DTOs.Auth;
+
+public class LoginResponseDto
+{
+    public string AccessToken { get; set; } = string.Empty;
+
+    public string RefreshToken { get; set; } = string.Empty;
+}
