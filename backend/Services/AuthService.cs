16b39ed5cb36a3ea8e9a70b57b69b8ec71650c3f
Gaudy035 <miloszgaudy@gmail.com>
2026-07-07 13:27:19 +0200

    feat(backend) Add login functionc

--- backend/Services/AuthService.cs
+++ backend/Services/AuthService.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using System.Text;
 using backend.Data;
 using backend.Data.Entities;
+using backend.DTOs.Auth;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
@@ -51,7 +52,7 @@ public class AuthService: IAuthService
         return tokenHandler.WriteToken(token);
     }
 
-    private async Task<RefreshToken?> GenerateRefreshToken(int userId, string role)
+    private async Task<string?> GenerateRefreshToken(int userId, string role)
     {
         string[] roles = ["Admin", "Employee", "Client"];
         
@@ -79,7 +80,7 @@ public class AuthService: IAuthService
         _context.RefreshTokens.Add(newToken);
         await _context.SaveChangesAsync();
 
-        return newToken;
+        return newToken.TokenValue;
     }
 
     public async Task RevokeToken(string tokenValue)
@@ -96,5 +97,55 @@ public class AuthService: IAuthService
         refreshToken.RevokedAt = DateTime.UtcNow;
 
         await _context.SaveChangesAsync();
+    }
+
+    public async Task<LoginResponseDto?> Login(LoginDto dto)
+    {
+        var foundUser = await _context.Users
+            .Include(u => u.Client)
+            .Include(u => u.Employee)
+            .FirstOrDefaultAsync(u => u.Email == dto.Email);
+        
+        if (foundUser == null)
+        {
+            return null;
+        }
+        
+        string role;
+
+        if (foundUser.Client != null)
+        {
+            role = "Client";
+        } 
+        else if (foundUser.Employee != null)
+        {
+            role = foundUser.Employee.IsAdmin 
+                ? "Admin" 
+                : "Employee";
+        }
+        else
+        {
+            return null;
+        }
+
+        if (!BCrypt.Net.BCrypt.Verify(dto.Password, foundUser.Password))
+        {
+            return null;
+        }
+
+        var accessToken = GenerateAccessToken(foundUser.UserId, role);
+        var refreshToken = await GenerateRefreshToken(foundUser.UserId, role);
+
+        if (accessToken == null || refreshToken == null)
+        {
+            return null;
+        }
+
+        return new LoginResponseDto
+        {
+            AccessToken = accessToken,
+            RefreshToken = refreshToken,
+            Role = role
+        };
     }
 }
--- backend/Services/Interfaces/IAuthService.cs
+++ backend/Services/Interfaces/IAuthService.cs
@@ -1,6 +1,10 @@
+using backend.DTOs.Auth;
+
 namespace backend.Services;
 
 public interface IAuthService
 {
     Task RevokeToken(string tokenValue);
+
+    Task<LoginResponseDto?> Login(LoginDto dto);
 }
