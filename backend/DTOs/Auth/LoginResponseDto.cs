6a112f3d9991a070a71e198eb213b2de3449c5d9
Gaudy035 <miloszgaudy@gmail.com>
2026-07-07 12:40:24 +0200

    feat(backend): Login resposne dto now contains users role

--- backend/DTOs/Auth/LoginResponseDto.cs
+++ backend/DTOs/Auth/LoginResponseDto.cs
@@ -5,4 +5,6 @@ public class LoginResponseDto
     public string AccessToken { get; set; } = string.Empty;
 
     public string RefreshToken { get; set; } = string.Empty;
+
+    public string Role { get; set; } = string.Empty;
 }
