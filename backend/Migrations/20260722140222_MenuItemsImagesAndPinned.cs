7b1e6a1d906190b4f94dee71f4114267eaab1891
Gaudy035 <miloszgaudy@gmail.com>
2026-07-22 16:03:12 +0200

    chore(backend): Add migrations for images and pins

---
+++ backend/Migrations/20260722140222_MenuItemsImagesAndPinned.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MenuItemsImagesAndPinned : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "image_url",
+                table: "t_menu_item",
+                type: "character varying(2048)",
+                maxLength: 2048,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_pinned",
+                table: "t_menu_item",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image_url",
+                table: "t_menu_item");
+
+            migrationBuilder.DropColumn(
+                name: "is_pinned",
+                table: "t_menu_item");
+        }
+    }
+}
