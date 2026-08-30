06d5d911f9cd35b9da4cf5150b7ad47e7c81dd85
Gaudy035 <miloszgaudy@gmail.com>
2026-08-30 14:38:32 +0200

    feat(frontend): Add login form functionality to admin panel

--- frontend/src/components/admin-login-form.tsx
+++ frontend/src/components/admin-login-form.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import ModeToggle from './ui/mode-toggle';
+import { adminApiFetch } from '@/lib/api';
+import { useRouter } from 'next/navigation';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
@@ -11,11 +15,36 @@ import {
 } from '@/components/ui/card';
 import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import React, { useState } from 'react';
 
 export function AdminLoginForm({
   className,
   ...props
 }: React.ComponentProps<'div'>) {
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError(null);
+
+    const formData = new FormData(event.currentTarget);
+    const payload = Object.fromEntries(formData.entries());
+
+    try {
+      await adminApiFetch('/admin/auth/login', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+      });
+
+      router.push('/admin/');
+      router.refresh();
+    } catch (err: any) {
+      console.log('API error' + err?.message);
+      setError(err?.message || 'Incorrect credentials');
+    }
+  };
+
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
@@ -29,13 +58,14 @@ export function AdminLoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor='email'>Email</FieldLabel>
                 <Input
                   id='email'
                   type='email'
+                  name='email'
                   placeholder='name@restaurant.com'
                   required
                 />
@@ -44,7 +74,7 @@ export function AdminLoginForm({
                 <div className='flex items-center'>
                   <FieldLabel htmlFor='password'>Password</FieldLabel>
                 </div>
-                <Input id='password' type='password' required />
+                <Input id='password' type='password' name='password' required />
               </Field>
               <Field>
                 <Button type='submit'>Login</Button>
