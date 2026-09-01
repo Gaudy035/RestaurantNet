f5cc392a24031dbd8b6e9fbf71856eee928eedb6
Gaudy035 <miloszgaudy@gmail.com>
2026-09-01 16:02:53 +0200

    feat(frontend): Add clients fetching component

---
+++ frontend/src/components/clients/ClientsMain.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import ClientData from '@/interfaces/ClientData';
+import { adminApiFetch } from '@/lib/api';
+import { useState, useEffect } from 'react';
+
+export default function AdminClientsMain({
+  clientData,
+}: {
+  clientData?: string;
+}) {
+  const [clients, setClients] = useState<ClientData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const endpoint = clientData
+      ? `/admin/user/clients?param=${clientData}`
+      : '/admin/user/clients';
+
+    adminApiFetch(endpoint, { method: 'GET', cache: 'no-store' })
+      .then((data) => setClients(data))
+      .catch((e) => setError(e));
+
+    setLoading(false);
+  }, [clientData]);
+
+  return (
+    <div>
+      <h1>{error ? error : null}</h1>
+      {loading ? 'loading' : clientData}
+    </div>
+  );
+}
