b5aeac45d19f9d69248beb6b9b733879925a3ce9
Gaudy035 <miloszgaudy@gmail.com>
2026-09-03 20:46:30 +0200

    feat(frontend): Add client card component

---
+++ frontend/src/components/clients/ClientCard.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { adminApiFetch } from '@/lib/api';
+import ClientData from '@/interfaces/ClientData';
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+import { Button } from '../ui/button';
+import React from 'react';
+
+export default function ClientCard({
+  clientData,
+  isAdmin,
+  setError,
+  handleDelete,
+}: {
+  clientData: ClientData;
+  isAdmin: boolean;
+  setError: React.Dispatch<React.SetStateAction<string | null>>;
+  handleDelete: (userId: string) => void;
+}) {
+  const deleteUser = async (userId: string) => {
+    setError(null);
+    try {
+      await adminApiFetch(`/admin/user/clients/${userId}`, {
+        method: 'DELETE',
+      });
+      handleDelete(userId);
+    } catch (e: any) {
+      setError(e?.message);
+      console.log(e?.message);
+    }
+  };
+
+  return (
+    <Card className='flex w-full py-4'>
+      <CardHeader className='flex flex-row justify-between items-center'>
+        <div className='flex flex-col justify-center items-start gap-2'>
+          <CardTitle>
+            {clientData.firstName} {clientData.lastName}
+          </CardTitle>
+          <CardDescription className='flex flex-row justify-center items-center gap-4'>
+            <p>Email: {clientData.email}</p>
+            <p>Phone number: {clientData.phoneNumber}</p>
+          </CardDescription>
+        </div>
+        {isAdmin ? (
+          <CardAction>
+            <Button
+              className='font-semibold bg-destructive hover:text-destructive hover:border-destructive hover:bg-primary'
+              size={'lg'}
+              onClick={() => {
+                if (
+                  window.confirm(
+                    `Delete user: ${clientData.firstName} ${clientData.lastName} (ID: ${clientData.userId})?`,
+                  )
+                ) {
+                  deleteUser(clientData.userId);
+                }
+              }}
+            >
+              Delete
+            </Button>
+          </CardAction>
+        ) : null}
+      </CardHeader>
+    </Card>
+  );
+}
