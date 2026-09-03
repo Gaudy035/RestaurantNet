b70a6c63105dc9f63a6858e12c2ea44298d4a638
Gaudy035 <miloszgaudy@gmail.com>
2026-09-03 20:46:56 +0200

    feat(frontend): Add client cards to main clients component

--- frontend/src/components/clients/ClientsMain.tsx
+++ frontend/src/components/clients/ClientsMain.tsx
@@ -4,6 +4,7 @@ import ClientData from '@/interfaces/ClientData';
 import { adminApiFetch } from '@/lib/api';
 import { useState, useEffect } from 'react';
 import { useEmployee } from '@/lib/employee-context/employee-context';
+import ClientCard from './ClientCard';
 
 export default function AdminClientsMain({
   clientData,
@@ -16,6 +17,10 @@ export default function AdminClientsMain({
 
   const EmployeeContext = useEmployee();
 
+  const handleClientDelete = (userId: string) => {
+    setClients((prev) => prev.filter((c) => c.userId !== userId));
+  };
+
   useEffect(() => {
     setLoading(true);
 
@@ -25,14 +30,34 @@ export default function AdminClientsMain({
 
     adminApiFetch(endpoint, { method: 'GET', cache: 'no-store' })
       .then((data) => setClients(data))
-      .catch((e) => setError(e))
+      .catch((e: any) => setError(e?.message))
       .finally(() => setLoading(false));
   }, [clientData]);
 
+  console.log(EmployeeContext);
+
   return (
-    <div>
-      <h1>{error ? error : null}</h1>
-      {loading ? 'loading' : clientData}
+    <div className='flex justify-center flex-col items-center'>
+      <p className='text-destructive'>{error ? error : null}</p>
+      {loading ? (
+        <p>Loading clients data...</p>
+      ) : clients.length > 0 ? (
+        // Clients found
+        <div className='flex flex-col m-8 w-full justify-center items-center gap-4'>
+          {clients.map((c) => (
+            <ClientCard
+              key={c.userId}
+              clientData={c}
+              isAdmin={EmployeeContext.employeeData?.isAdmin ?? false}
+              setError={setError}
+              handleDelete={handleClientDelete}
+            />
+          ))}
+        </div>
+      ) : (
+        // No clients found
+        <p>No clients found</p>
+      )}
     </div>
   );
 }
