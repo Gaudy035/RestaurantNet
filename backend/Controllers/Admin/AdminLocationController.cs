2b86c077b3a3add9054d137debb3e3a44ee23e58
Gaudy035 <miloszgaudy@gmail.com>
2026-09-08 15:18:11 +0200

    feat(backend): add location controller for admin panel

---
+++ backend/Controllers/Admin/AdminLocationController.cs
@@ -0,0 +1,56 @@
+using backend.DTOs.Locations;
+using backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("admin/locations")]
+public class AdminLocationsController: ControllerBase
+{
+    private readonly ILocationService _locationService;
+
+    public AdminLocationsController(ILocationService locationService)
+    {
+        _locationService = locationService;
+    }
+
+    [Authorize(Roles = "Admin,Employee")]
+    [HttpGet()]
+    public async Task<IActionResult> GetLocations()
+    {
+        var foundLocations = await _locationService.GetLocations();
+
+        return Ok(foundLocations);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("${int:locationId}")]
+    public async Task<IActionResult> FindLocation([FromRoute] int locationId)
+    {
+        var location = await _locationService.FindLocation(locationId);
+
+        if (location == null)
+        {
+            return NotFound(new { detail = "Location not found" });
+        }
+
+        return Ok(location);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost()]
+    public async Task<IActionResult> CreateLocation(LocationCreateDto dto)
+    {
+        var newLocation = await _locationService.CreateLocation(dto);
+
+        if (newLocation == null)
+        {
+            return StatusCode(500);
+        }
+
+        return Ok(newLocation);
+    }
+
+}
